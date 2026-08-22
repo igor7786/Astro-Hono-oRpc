3ac159de42b7d9c50cbd6f0a1852ed4f8541bea6
Igor7786 <grimuta60@gmail.com>
2026-08-22 03:23:17 +0000

    refactor: remove unused schemas and clean up related exports

--- src/lib/drizzle/sqlite/schemas/index.ts
+++ src/lib/drizzle/sqlite/schemas/index.ts
@@ -1,3 +1,1 @@
-export * from '@/lib/drizzle/sqlite/schemas/og.images';
 export * from '@/lib/drizzle/sqlite/schemas/test';
-export * from '@/lib/drizzle/sqlite/schemas/hashes';
--- src/server/schemas/tests-schema/test.schema.ts
+++ src/server/schemas/tests-schema/test.schema.ts
@@ -5,8 +5,7 @@ export const testSchema = z.object({
     .string()
     .trim()
     .min(3, { message: 'Name must be at least 3 characters long' })
-    .max(200, { message: 'Name must be at most 200 characters long' })
-    .default('John Doe'),
+    .max(200, { message: 'Name must be at most 200 characters long' }),
 });
 
 export type TestInput = z.infer<typeof testSchema>;
