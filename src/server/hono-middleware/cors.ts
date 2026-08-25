909e830a8cf6a34b54014e54a62139b5df0d2925
Igor7786 <grimuta60@gmail.com>
2026-08-25 00:49:48 +0000

    feat(cors, csrf): update CORS and CSRF middleware to allow multiple origins

--- src/server/hono-middleware/cors.ts
+++ src/server/hono-middleware/cors.ts
@@ -4,7 +4,10 @@ import { cors } from 'hono/cors';
 import { envServer } from '@/lib/env/server.env';
 
 const corsMiddleware = cors({
-  origin: [envServer.PUBLIC_URL],
+  origin: (origin) => {
+    const allowed = [envServer.PUBLIC_URL, 'http://localhost:4321', 'http://localhost:4322'];
+    return allowed.includes(origin) ? origin : envServer.PUBLIC_URL;
+  },
   credentials: true,
   allowHeaders: ['Content-Type', 'Authorization'],
   allowMethods: ['POST', 'GET', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
--- src/server/hono-middleware/csrf.ts
+++ src/server/hono-middleware/csrf.ts
@@ -1,4 +1,9 @@
 import { csrf } from 'hono/csrf';
 
-const origin = ['https://fast-web-tech.co.uk', 'http://localhost:4321', 'http://localhost:4322'];
+const origin = [
+  'https://fast-web-tech.co.uk',
+  'https://www.fast-web-tech.co.uk',
+  'http://localhost:4321',
+  'http://localhost:4322',
+];
 export const csrfMiddleware = csrf({ origin });
